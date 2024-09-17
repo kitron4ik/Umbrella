ae0cdbf455f7d20574cf0e644d739783560328ba
Kitron4ik <ggetman.v.i22@gmail.com>
2024-09-17 11:05:16 +0300

    Догрузил БД MySQL

---
+++ MyDB/umbrella_health_card.sql
@@ -0,0 +1,62 @@
+-- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
+--
+-- Host: localhost    Database: umbrella
+-- ------------------------------------------------------
+-- Server version	8.0.39
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `health_card`
+--
+
+DROP TABLE IF EXISTS `health_card`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `health_card` (
+  `idhealth_card` int NOT NULL,
+  `pacients_idpacients` int NOT NULL,
+  `hospitals_idhospitals` int NOT NULL,
+  `consent` varchar(500) DEFAULT NULL,
+  `accounting_date` date DEFAULT NULL,
+  `diagnoses` varchar(45) DEFAULT NULL,
+  `sick_leave` varchar(500) DEFAULT NULL,
+  `checks` date DEFAULT NULL,
+  `expertise_plan` varchar(500) DEFAULT NULL,
+  `medecine` varchar(500) DEFAULT NULL,
+  PRIMARY KEY (`idhealth_card`),
+  KEY `fk_health_card_pacients1_idx` (`pacients_idpacients`),
+  KEY `fk_health_card_hospitals1_idx` (`hospitals_idhospitals`),
+  CONSTRAINT `fk_health_card_hospitals1` FOREIGN KEY (`hospitals_idhospitals`) REFERENCES `hospitals` (`idhospitals`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `fk_health_card_pacients1` FOREIGN KEY (`pacients_idpacients`) REFERENCES `pacients` (`idpacients`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `health_card`
+--
+
+LOCK TABLES `health_card` WRITE;
+/*!40000 ALTER TABLE `health_card` DISABLE KEYS */;
+/*!40000 ALTER TABLE `health_card` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-09-17 11:03:33
