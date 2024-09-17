4cf22a1aedbb6a50cd337c94726fffd022d13c4d
Kitron4ik <ggetman.v.i22@gmail.com>
2024-09-17 14:21:32 +0300

    Alpha0.1

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_mysqldb import MySQL
 
 app = Flask(__name__, static_folder='./templates/static')
+app.secret_key = 'your_secret_key'  # Необходимо для использования flash-сообщений
 
 # Настройки для подключения к MySQL
 app.config['MYSQL_HOST'] = 'localhost'
@@ -18,38 +19,66 @@ def main():
 
 @app.route('/login', methods=['POST'])
 def login():
-    if request.method == 'POST':
-        name = request.form['name']
-        email = request.form['email']
-        building_code = request.form['buildingCode']
-        role_code = request.form['roleCode']
-        role = request.form['role']
-        password = request.form['password']
+    email = request.form['email']
+    password = request.form['password']
 
-        # МайЭСКЮЭЛЬ
-        cur = mysql.connection.cursor()
-        cur.execute("""
-            INSERT INTO reg (name, email, building_code, role_code, role, password) 
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """, (name, email, building_code, role_code, role, password))
-        
-        mysql.connection.commit()
-        cur.close()
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM reg WHERE email = %s AND password = %s", (email, password))
+    user = cur.fetchone()
+    cur.close()
 
-        # Роли
+    if user:
+        # Логика для перенаправления в зависимости от роли
+        role = user[6]  # Индекс может изменяться в зависимости от структуры таблицы
         if role == "Доктор":
             return redirect(url_for('doctor_page'))
         elif role == "Пациент":
             return redirect(url_for('patient_page'))
         else:
+            flash('Неверная роль пользователя')
             return redirect(url_for('main'))
+    else:
+        flash('Неверный email или пароль')
+        return redirect(url_for('main'))
 
-# Страница пациента
+@app.route('/register', methods=['POST'])
+def register():
+    name = request.form['name']
+    email = request.form['email']
+    building_code = request.form['buildingCode']
+    role_code = request.form['roleCode']
+    role = request.form['role']
+    password = request.form['password']
+    cur = mysql.connection.cursor()
+
+    cur.execute("SELECT * FROM reg WHERE email = %s", (email,))
+    existing_user = cur.fetchone()
+
+    
+
+    if existing_user:
+        flash('Пользователь с таким email уже зарегистрирован.')
+        cur.close()
+        return redirect(url_for('main'))
+    cur.execute("""
+        INSERT INTO reg (name, email, building_code, role_code, role, password) 
+        VALUES (%s, %s, %s, %s, %s, %s)
+    """, (name, email, building_code, role_code, role, password))
+    mysql.connection.commit()
+    cur.close()
+
+    if role == "Доктор":
+         return redirect(url_for('doctor_page'))
+    elif role == "Пациент":
+        return redirect(url_for('patient_page'))
+    else:
+        flash('Неверная роль пользователя')
+        return redirect(url_for('main'))
+
 @app.route('/patient')
 def patient_page():
     return render_template("patient.html")
 
-# Страница доктора
 @app.route('/doctor')
 def doctor_page():
     return render_template("doctor.html")
